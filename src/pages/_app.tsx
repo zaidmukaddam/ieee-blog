3bae10bc9fa76a998c678d2cc566cdccb2c908ee
Zaid Mukaddam <zaidaltafmukaddam@gmail.com>
2023-03-06 20:46:22 +0530

    fix typescript version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
           }
         `}
       </style>
-      <Analytics mode={'production'} />;
+      <Analytics mode={'production'} />
       <Component {...pageProps} />
     </>
   )
